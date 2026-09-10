0f20d91883ec4cea32fdb983fc8ae67e1f317658
Denis <bloguserprogrammer1@gmail.com>
2026-09-10 21:22:09 +0300

    update v 1.3

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -16,6 +17,7 @@ public class BaseTest {
 
     @Before
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
